cf402e182e9ed10b7b6f7e1c5168f622e6efb793
Joao Gato <joaovitorara98@gmail.com>
2025-06-14 10:38:04 -0300

    wip

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { getAuthToken } from './auth';
 
 const api = axios.create({
-    baseURL: "http://localhost:3333",
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3333',
 });
 
 api.interceptors.request.use(async (config) => {
